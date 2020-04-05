8a00894c63ccbe1864fe0fb363704573afbbe02b
nsubotovsky <n.subotovsky@gmail.com>
2020-04-05 13:23:31 -0400

    fix linux paths

--- main.r
+++ main.r
@@ -3,7 +3,7 @@
 rm(list = ls())
 gc()
 
-start.modules <- c("~/cloud/cloud1/recuperatorio/loader.recuperatorio.r", # linux location
+start.modules <- c("~/dm_finanzas/loader.recuperatorio.r", # linux location
                    "C:/Users/Luxor/Documents/GitHub/Test_01/dm_finanzas/loader.recuperatorio.r" # windows location
 )
 
