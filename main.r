8717cfb78e2678a5266d5cab2f2fddbfbd1df074
nsubotovsky <54336861+nsubotovsky@users.noreply.github.com>
2020-04-05 00:57:32 -0300

    Clear memory before anything else

--- main.r
+++ main.r
@@ -1,5 +1,7 @@
 ########### Boilerplate code to auto-load correct module ##############
 
+rm(list = ls())
+
 start.modules <- c("~/cloud/cloud1/recuperatorio/loader.recuperatorio.r", # linux location
                    "C:/Users/Luxor/Documents/GitHub/Test_01/dm_finanzas/loader.recuperatorio.r" # windows location
 )
