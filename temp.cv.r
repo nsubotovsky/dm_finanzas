5ab1755be590b78df37d73a57f0136a3f16c407d
nsubotovsky <54336861+nsubotovsky@users.noreply.github.com>
2020-04-16 01:10:12 -0300

    runningCV with multiple seeds and avg them

--- temp.cv.r
+++ temp.cv.r
@@ -24,7 +24,7 @@ globalenv()$log.debug('Starting lineaDeMuerte original')
 
 library( "data.table" )
 library( "xgboost" )
-
+library(ggplot2)
 
 
 
@@ -33,11 +33,6 @@ datasets <- ( globalenv()$get.train.df() %>% split.train.test.df(0.7, clase) )
 train.df <- globalenv()$DfHolder$new(datasets$train)
 validate.df <- globalenv()$DfHolder$new(datasets$test)
 
-
-set.seed( 102191999 ) #mi querida random seed, para que las corridas sean reproducibles
-
-
-globalenv()$log.debug('training...')
 
 
 testFunc <- function(preds, dtrain)
@@ -49,34 +44,53 @@ testFunc <- function(preds, dtrain)
 
 
 
-aa <- xgb.cv(data=train.df$as.xgb.train(),
-             nfold=5,
-             objective= "binary:logistic",
-             tree_method= "hist",
-             max_bin= 31,
-             base_score=train.df$mean,
-             eta= 0.04,
-             nrounds= 500, 
-             colsample_bytree= 0.6,
-             stratified=TRUE,
-             maximize = TRUE,
-             #eval_metric='auc',
-             #evals=list(pruebaA=validate.df$as.xgb.train()),
-             feval=testFunc
+
+run.cv <- function(seed)
+{
+    set.seed( seed )
+    aa <- xgb.cv(data=train.df$as.xgb.train(),
+                 nfold=5,
+                 objective= "binary:logistic",
+                 tree_method= "hist",
+                 max_bin= 31,
+                 base_score=train.df$mean,
+                 eta= 0.04,
+                 nrounds= 500, 
+                 colsample_bytree= 0.6,
+                 stratified=TRUE,
+                 maximize = TRUE,
+                 feval=testFunc
+    )
+    return(aa$evaluation_log)
+}
+
+seedVector <- data.table( seed=c(12345,154784,12369))
+
+rawResults <- seedVector %>% mutate( result=map( seed, run.cv ))
+
+
+avgResults <- rawResults %>%
+    unnest( result ) %>%
+    group_by(iter) %>%
+    summarize(mean=mean(test_suboMetrics_mean), sd=mean(test_suboMetrics_std))
+
+
+print( avgResults %>% ggplot(aes(x=iter) ) + #, color=who
+           geom_line( aes(y=mean, color='blue' )) +
+           geom_line( aes(y=sd, color='red' )) +
+           ggtitle('gain and sd vs iteration')
 )
 
-library(ggplot2)
 
-print ( ggplot( aa$evaluation_log, aes(x=iter) ) + #, color=who
-            geom_line( aes(y=test_suboMetrics_std, color='red' )) +
-            geom_line( aes(y=test_suboMetrics_mean, color='blue' )) +
-            # xlim(0.005,0.1) +
-            # ylim(8000000,11000000) +
-            #ylim(8500000,9500000) +
-            ggtitle('My Title'))
-        
-        
+top5Values <- (avgResults %>% arrange(mean) %>% tail(5))
+
+bestIterationAvg <- top5Values$iter %>% mean()
+bestIterationSd <- top5Values$iter %>% sd()
 
 
 
-#results <- data.table( seeds=c(12345,154784,12369,45411) )
+
+
+
+
+
