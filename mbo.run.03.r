134c049c4acfefaa2495068cccd5ffa5de751403
nsubotovsky <n.subotovsky@gmail.com>
2020-04-24 10:30:14 -0400

    Updating results for MBO run 3

--- mbo.run.03.r
+++ mbo.run.03.r
@@ -123,3 +123,42 @@ interpert.mbo <- function(file)
 
 
 print(run)
+
+
+ggplot(interpert.mbo(mbo.file) %>% filter(y > 60), aes(x = eta, y = colsample_bytree, color=y))+ geom_point() + xlim(0,.1) + ylim(0,1) + scale_color_gradient(low="blue", high="red")
+
+#
+#
+#  Results for run...
+#
+# > print(run)
+# Recommended parameters:
+#     eta=0.0579; colsample_bytree=0.191
+# Objective: y = 61.783
+# 
+# Optimization path
+# 6 + 600 entries in total, displaying last 10 (or less):
+#     eta colsample_bytree        y dob eol error.message exec.time           ei error.model train.time prop.type propose.time
+# 597 0.05039946        0.4144321 54.33529 591  NA          <NA>    48.331 -0.001891943        <NA>      9.695 infill_ei        0.136
+# 598 0.07137908        0.1880172 55.73662 592  NA          <NA>    47.202 -0.001891917        <NA>      7.918 infill_ei        0.137
+# 599 0.01806925        0.3960633 55.67461 593  NA          <NA>    48.869 -0.001875873        <NA>      7.474 infill_ei        0.136
+# 600 0.07201178        0.7180483 56.99677 594  NA          <NA>    48.605 -0.001860280        <NA>      8.664 infill_ei        0.137
+# 601 0.05909428        0.1856778 55.98733 595  NA          <NA>    49.213 -0.003565353        <NA>      9.695 infill_ei        0.142
+# 602 0.06907221        0.3835352 59.91555 596  NA          <NA>    47.439 -0.001829380        <NA>      9.065 infill_ei        0.140
+# 603 0.05782309        0.3988809 58.39810 597  NA          <NA>    47.674 -0.001874435        <NA>      5.869 infill_ei        0.134
+# 604 0.07157012        0.3794043 57.42429 598  NA          <NA>    48.647 -0.001878765        <NA>     10.390 infill_ei        0.134
+# 605 0.07838905        0.9248204 53.82464 599  NA          <NA>    48.088 -0.001868093        <NA>      6.210 infill_ei        0.134
+# 606 0.06422598        0.5258294 55.15999 600  NA          <NA>    47.494 -0.001878361        <NA>      5.794 infill_ei        0.148
+# se     mean
+# 597 1.819706 56.85866
+# 598 1.821107 56.85443
+# 599 1.820157 56.85256
+# 600 1.819273 56.85060
+# 601 1.537309 58.01233
+# 602 1.816594 56.84940
+# 603 1.819374 56.85450
+# 604 1.818950 56.85706
+# 605 1.817590 56.85800
+# 606 1.820262 56.85298
+
+
